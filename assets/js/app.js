a30519a8ebf9a55d1e0393966239d9609bbddad0
8GutiMu <you@example.com>
2017-08-16 15:24:16 -0500

    inicio de firebase con inicio con logIn,cancelar btn

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,48 @@ $( document ).ready(function() {
 //   }
 // );
 //
+
+
+  // Initialize Firebase
+  var config = {
+  	apiKey: "AIzaSyC1F-frBdAU_Hm8TgZ6GuS9HDedgDGfn5o",
+  	authDomain: "hacer-login-6c332.firebaseapp.com",
+  	databaseURL: "https://hacer-login-6c332.firebaseio.com",
+  	projectId: "hacer-login-6c332",
+  	storageBucket: "hacer-login-6c332.appspot.com",
+  	messagingSenderId: "31841737069"
+  };
+
+  firebase.initializeApp(config);
+
+  var loggearConFacebook = function () {
+
+  	var provider = new firebase.auth.FacebookAuthProvider();
+  	getDataUser(provider)
+  }
+
+
+
+  var getDataUser = function (provider) {
+  	firebase.auth().signInWithPopup(provider).then(function (result) {
+  		// This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  		var token = result.credential.accessToken;
+  		// The signed-in user info.
+  		var user = result.user;
+  		console.log(user)
+//  		document.getElementById("userF").textContent = user.displayName
+
+  	}).catch(function (error) {
+  		// Handle Errors here.
+  		var errorCode = error.code;
+  		var errorMessage = error.message;
+  		// The email of the user's account used.
+  		var email = error.email;
+  		// The firebase.auth.AuthCredential type that was used.
+  		var credential = error.credential;
+  		// ...
+  		console.log("error", errorMessage)
+  	});
+  }
+  document.getElementById("btnF").addEventListener("click", loggearConFacebook)
+
--- assets/js/data.js
+++ assets/js/data.js
@@ -114,18 +114,23 @@ $(document).on("click", ".canje-list-btn", pasarACANJEAR)
 $(document).on("click", ".whish-list-btn", pasarAwhishlist)
 
 function pasarACANJEAR() {
+	
+	
 	swal({
 			title: "¿QUIERES CANJEAR TUS PUNTOS POR ESTE ARTÍCULO?",
 			text: "Recuerda que todos los artículos estan sujetos a condiciones ",
 			type: "success",
-			showCancelButton: true,
-			confirmButtonColor: "#DD6B55",
+			showCancelButton: "true",
+			confirmButtonColor: "#1de9b6",
 			confirmButtonText: "SI, LO QUIERO",
+			cancelButtonText: "Cancelar",
 			closeOnConfirm: false
 		},
 		function () {
 			swal("LISTO", "ESTE ARTICULO APARECE AHORA PERFIL.", "success");
 		});
+
+	
 }
 
 function pasarAwhishlist() {
@@ -196,6 +201,8 @@ var mostrarTarjetas = function (recompensas) {
 		$divCr.append($spanR, $pNom, $pPts, $pInf, $btnW, $btnC);
 		$div.append($divI, $divCc, $divCr)
 
+		
+		
 		$contenedor.append($div);
 
 
